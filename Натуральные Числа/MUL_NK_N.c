d11a9946da61825ed9a34fb6a853e5ea43ac8388
greg <greg.kargin@gmail.com>
2015-05-10 21:31:12 +0300

    Update MUL_NK_N.c

---
+++ Натуральные Числа/MUL_NK_N.c
@@ -0,0 +1,12 @@
+struct NATURAL MUL_Nk_N(struct NATURAL A, int k)
+{
+        int i, j, new_index;
+        new_index = A.index + k;
+        A.number = (short int*) realloc (A.number, (new_index + 1) * sizeof (short int));
+        for (i = A.index; i >= 0; i--)
+                A.number[i+k] = A.number[i];
+        for (i = 0; i < k; i++)
+                A.number[i] = 0;
+        A.index = new_index;
+        return A;
+}
