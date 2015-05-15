9d5024cdb34ee47f69d66e9160cd80827d81afd5
adegtyarev33 <adegtyarev.pfn@gmail.com>
2015-05-16 03:34:55 +0400

    Update and rename MUL_ZZ_Z to MUL_ZZ_Z.c
    Добавлены ключевые слова struct; удалена из файла структура INTEGER

--- Целые Числа/MUL_ZZ_Z.c
+++ Целые Числа/MUL_ZZ_Z.c
@@ -11,19 +11,9 @@
 #include "MUL_NN_N.c"
 #include "MUL_Z-_Z.c"
 
-struct INTEGER
-{
-  struct NATURAL
-    {
-    int*number;//указатель на массив цифр числа
-    int index; //номер старшего разряда
-    } natural_part;
-  short sign;//знак числа (1 — минус, 0 — плюс)
-};
-
-INTEGER MULL_ZZ_Z(INTEGER factor1, INTEGER factor2)  // result = factor1 * factor2
+struct INTEGER MULL_ZZ_Z(struct INTEGER factor1, struct INTEGER factor2)  // result = factor1 * factor2
   {
-  INTEGER result;
+  struct INTEGER result;
   result.sign = 0;
   result.natural_part = MUL_NN_N(ABS_Z_N(factor1), ABS_Z_N(factor2));
   if (POZ_Z_D(a) * POZ_Z_D(b) == 2)
