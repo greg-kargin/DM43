4bc0cc9d8f8e8ab010760b7f1502f51c6164440c
wildchama <wildchama@users.noreply.github.com>
2015-05-15 23:37:24 +0400

    Update MUL_ND_N.c

--- Натуральные Числа/MUL_ND_N.c
+++ Натуральные Числа/MUL_ND_N.c
@@ -1,40 +1,20 @@
-
-int MUL_ND_N(int *mas, int *index, short *znak, int k)
-// mas - адрес первого элемента структуры, хранящей число (задом - наперед)
-// index - количество цифр в числе
-// znak - знак числа (0 - положительное, 1 - отрицательное)
-// k - число, на которое производится умножение
-//
+struct NATURAL MUL_ND_N(struct NATURAL a, int k)
 {
-    int * res; // структура получившегося числа
-    int p; // для хранения результата умножения разряда на k
-    int i,j; // для цикла
-    res=(int*)malloc((*index)*sizeof(int)); // обнуление res
-    for (i=0;i<(*index);i++)
-        *(res+i)=0;
-    
-    for (i=0; i<(*index); i++)
-    {
-        p=*(mas+i) * k;
-        for (j=i; p>0; j++)
-        {
-            if (j>(*index)) // если вылезли за пределы res
-            {
-                res = (int*)realloc(res, ((*index)+ 1 )*sizeof(int)); // добавляем новый элемент
-                (*index)++ ;
-                *(res + (*index)) = 0; // обнуляем его
-            }
-            *(res+j) += p % 10;
-            p /= 10;
-        }
-        
-    }
-    if (k==1)
-    {
-        (*znak)*=1;
-        (*znak) = (*znak) % 2;
-    }
-        
-    mas = res;
-    return 1;
+	struct NATURAL res; int i, p;
+	if (!k)
+	{
+		res.index = 1;
+		res.number = (int *)calloc(res.index, sizeof(int));
+		return res;
+	}
+	res.index = a.index + 1;
+	res.number = (int *)calloc(res.index, sizeof(int));
+	for (i = 0; i < a.index; i++)
+	{
+		p = *(a.number + i) * k;
+		*(res.number + i + 1) += p / 10;
+		*(res.number + i) += p % 10;
+	}
+	if (!(*(res.number + a.index))) res.index--;
+	return res;
 }
