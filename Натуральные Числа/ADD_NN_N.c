ff45c05b303235fb3d8dd76f5c5046ec6b805cad
adegtyarev33 <adegtyarev.pfn@gmail.com>
2015-05-16 02:53:36 +0400

    Update ADD_NN_N.c
    Изменен формат входных данных: изменение исходных данных в ходе работы данной функции нежелательно (мешает работе других функций!)

--- Натуральные Числа/ADD_NN_N.c
+++ Натуральные Числа/ADD_NN_N.c
@@ -3,23 +3,25 @@
 // Сидельников, Яшина - 4307
 
 // Выполняет сложение натуральных чисел
-void ADD_NN_N(struct NATURAL* a, struct NATURAL* b)
+struct NATURAL ADD_NN_N(struct NATURAL a, struct NATURAL b)
 {
-int i, flag = 0, // flag - проверка на успешность выполнения операции
-for (i = 0; i < ((*a).index) && !flag; i++)
-{
-	if (((*a).number[i] + (*b).number[i]) > 9) // проверка, нужно ли делать перенос
-	{
-		(*a).number[i] = (((*a).number[i] + (*b).number[i]) - 10);
-		if (i == (*a).index - 1) // является ли цифра последней при проверке
+	int i, flag = 0; // flag - проверка на успешность выполнения операции
+    for (i = 0; i < (a.index) && !flag; i++)
 		{
-			(*a).number[(*a).index] = 1;
-			((*a).index)++;  // увеличиваем кол-во разрядов на 1
-			flag = 1;
-		}   
-		else // "единица в уме", если цифра не последняя 
-		(*a).number[i+1]++;
-	}
-	else
-	(*a).number[i] = (*a).number[i] + (*b).number[i]; // цифры складываются
+      if ((a.number[i] + b.number[i]) > 9) // проверка, нужно ли делать перенос
+			{
+        a.number[i] = ((a.number[i] + b.number[i]) - 10);
+				if (i == (*a).index - 1) // является ли цифра последней при проверке
+				{
+          a.number[a.index] = 1;
+          (a.index)++;  // увеличиваем кол-во разрядов на 1
+					flag = 1;
+				}
+				else // "единица в уме", если цифра не последняя 
+          a.number[i + 1]++;
+			}
+			else
+        a.number[i] = a.number[i] + b.number[i]; // цифры складываются
+		}
+    return a;
 }
