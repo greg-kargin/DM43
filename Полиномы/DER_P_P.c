cdfaf2da3c27132d5ae73ba37746f6fc0245d9d9
wildchama <wildchama@users.noreply.github.com>
2015-05-15 23:42:43 +0400

    Update DER_P_P.c

--- Полиномы/DER_P_P.c
+++ Полиномы/DER_P_P.c
@@ -1,8 +1,17 @@
-POLYNOMIAL DER_P_P(POLYNOMIAL polinom)
+struct POLYNOMIAL DER_P_P(struct POLYNOMIAL a)
 {
-for(int i = polinom.degree;i > 0;i--)
-polinom.factors[i].numerator *= i;
-polinom.degree —;
-return polinom;
-
+	struct POLYNOMIAL res;
+	int i, j;
+	res.degree = a.degree - 1;
+	res.factors = (struct RATIONAL *)malloc(a.degree * sizeof(struct RATIONAL));
+	for (i = 0; i < a.degree; i++)
+	{
+		res.factors[i].numerator.index = a.factors[i + 1].numerator.index;
+		res.factors[i].numerator = MUL_ND_N(a.factors[i + 1].numerator, i + 1);
+		res.factors[i].denominator.index = a.factors[i + 1].denominator.index;
+		res.factors[i].denominator.number = (int *)malloc(res.factors[i].denominator.index * sizeof(int));
+		for (j = 0; j < res.factors[i].denominator.index; j++)
+			res.factors[i].denominator.number[j] = a.factors[i + 1].denominator.number[j];
+	}
+	return res;
 }
