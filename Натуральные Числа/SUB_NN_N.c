7363c482ab2b89f7e441b03564b8aeef6dc20996
wildchama <wildchama@users.noreply.github.com>
2015-05-15 23:38:43 +0400

    Update SUB_NN_N.c

--- Натуральные Числа/SUB_NN_N.c
+++ Натуральные Числа/SUB_NN_N.c
@@ -15,57 +15,68 @@ rec - результат вычитания
 группа 4305
 */
 
-NATURAL SUB_NN_N(NATURAL a, NATURAL b)
+struct NATURAL SUB_NN_N(struct NATURAL a, struct NATURAL b) //пойдёт
 {
-NATURAL first, second, res;
-bool flag = false;
-int n, i;
-//Находим большее/меньшее число
-n=COM_NN_D(a, b);
-if (n == 0)
-*(res.number) = 0;
-if (n == 2)
-{
-first = a;
-second = b;
-}
-else
-{
-first = b;
-second = a;
-}
+	struct NATURAL first, second, res;
+	bool flag = true;
+	int n, i;
+	//Находим большее/меньшее число
+	n = COM_NN_D(a, b);
+	if (n == 0)
+	{
+		res.number = (int *)malloc(sizeof(int));
+		*(res.number) = 0;
+		res.index = 1;
+	}
+
+	else
+	{
+		res.number = NULL;
+		if (n == 2)
+		{
+			first = a;
+			second = b;
+		}
+		else
+		{
+			first = b;
+			second = a;
+		}
 
 
-for (i = 0; i == second.index; i++)
-{
-if (*(first.number + i) >= *(second.number + i))
-*(res.number + i) = *(first.number + i) - *(second.number + i);
-else
-{
-*(first.number + (i + 1)) = *(first.number + (i + 1)) - 1;
-*(first.number + i) = *(first.number + i) + 10;
-*(res.number + i) = *(first.number + i) - *(second.number + i);
-}
-}
-for (i = second.index; i == first.index; i++)
-{
-if (*(first.number + i) < 0)
-{
-*(first.number + (i + 1)) = *(first.number + (i + 1)) - 1;
-*(first.number + i) = *(first.number + i) + 10;
-}
-}
+		for (i = 0; i < second.index; i++)
+		{
+			res.number = (int *)realloc(res.number, (i + 1)*sizeof(int));
+			if (*(first.number + i) < *(second.number + i))
+			{
+				(*(first.number + (i + 1)))--;
+				*(first.number + i) += 10;
+				*(res.number + i) = *(first.number + i) - *(second.number + i);
+			}
+			*(res.number + i) = *(first.number + i) - *(second.number + i);
+		}
+		for (i = second.index; i < first.index; i++)
+		{
+			if (*(first.number + i) < 0)
+			{
+				(*(first.number + (i + 1)))--;
+				*(first.number + i) += 10;
+			}
+		}
 
-for (i = first.index; i == second.index&&flag == false; i--)
-{
-if (*(first.number + i) == 0)
-first.index = first.index - 1;
-else
-flag == true;
-}
-for (i = second.index + 1; i == first.index; i++)
-*(res.number + i) = *(first.number + i);
-
-res.index = i;
-return res;
+		for (i = first.index; i > second.index && flag; i--)
+		{
+			if (*(first.number + i) == 0)
+				first.index = first.index - 1;
+			else
+				flag = false;
+		}
+		for (i = second.index; i < first.index; i++)
+		{
+			res.number = (int *)realloc(res.number, (i + 1)*sizeof(int));
+			*(res.number + i) = *(first.number + i);
+		}
+		res.index = i;
+	}
+	return res;
 }
