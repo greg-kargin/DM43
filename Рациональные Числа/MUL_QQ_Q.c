5d3e7999cbcff68718b05a0c335a49e82cdddf4c
adegtyarev33 <adegtyarev.pfn@gmail.com>
2015-05-16 03:49:32 +0400

    Update MUL_QQ_Q.c
    Сокращен алгоритм определения знака результата

--- Рациональные Числа/MUL_QQ_Q.c
+++ Рациональные Числа/MUL_QQ_Q.c
@@ -15,17 +15,11 @@ b - вторая дробь
 Сыромятников Михаил
 группа 4306
 */
-RATIONAL MUL_QQ_Q (RATIONAL a, RATIONAL b)
-  {
-  	RATIONAL c;
-  	c.numerator = MUL_ZZ_Z(a.numerator, b.numerator);
-  	c.denominator = MUL_ZZ_Z(a.denominator, b.denominator);
-  	if(a.sign == 1 && b.sign == 1)
-  	  c.sign = 0;
-  	else
-  	  if(a.sign == 1 || b.sign == 1)
-  	    c.sign = 1;
-  	  else
-  	    c.sign = 0;
-  	return c;
-  }
+struct RATIONAL MUL_QQ_Q(struct RATIONAL a, struct RATIONAL b)
+{
+	struct RATIONAL c;
+	c.numerator = MUL_ZZ_Z(a.numerator, b.numerator);
+	c.denominator = MUL_ZZ_Z(a.denominator, b.denominator);
+  c.sign = (a.sign == b.sign) ? 0 : 1;
+	return c;
+}
