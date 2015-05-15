7a13786b0c97cad55aafac8294ea5d98a29bd12b
sergysta <sergysta@users.noreply.github.com>
2015-05-16 01:06:55 +0300

    Update ADD_PP_P.c

---
+++ Полиномы/ADD_PP_P.c
@@ -0,0 +1,34 @@
+POLYNOMIAL ADD_PP_P(POLYNOMIAL first, POLYNOMIAL second, POLYNOMIAL rezult)
+{
+	rezult.factors = NULL;
+	int mem = 0;
+	if (first.degree >= second.degree)
+	{
+		rezult.degree = first.degree;
+		rezult.factors = (RATIONAL*) realloc(rezult.factors, rezult.degree);
+		for (int i = 0; i < second.degree; i++)
+		{
+			rezult.factors[i] = ADD_QQ_Q(first.factors[i].numerator, first.factors[i].denominator, first.factors[i].sign, second.factors[i].numerator, second.factors[i].denominator, second.factors[i].sign, rezult.factors[i].numerator, rezult.factors[i].denominator, rezult.factors[i].sign);
+			mem = i;
+		}
+		for (int i = mem; i < first.degree; i++)
+		{
+			rezult.factors[i] = first.factors[i];
+		}
+	}
+	else
+	{
+		rezult.degree = second.degree;
+		rezult.factors = (RATIONAL*)realloc(rezult.factors, rezult.degree);
+		for (int i = 0; i < first.degree; i++)
+		{
+			rezult.factors[i] = ADD_QQ_Q(first.factors[i].numerator, first.factors[i].denominator, first.factors[i].sign, second.factors[i].numerator, second.factors[i].denominator, second.factors[i].sign, rezult.factors[i].numerator, rezult.factors[i].denominator, rezult.factors[i].sign);
+			mem = i;
+		}
+		for (int i = mem; i < second.degree; i++)
+		{
+			rezult.factors[i] = second.factors[i];
+		}
+	}
+	return rezult;
+}
