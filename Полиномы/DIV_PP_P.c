631fdb4578e7b51e4cd364a4c400b62a3357e5ee
wildchama <wildchama@users.noreply.github.com>
2015-05-16 01:20:27 +0400

    Update DIV_PP_P.c

--- Полиномы/DIV_PP_P.c
+++ Полиномы/DIV_PP_P.c
@@ -1,38 +1,27 @@
 // Частное от деления многочлена на многочлен
 // Прототип - struct POLYNOMIAL DIV_PP_P (struct POLYNOMIAL, struct POLYNOMIAL)
-// Произвоидтся деление Px на Mx
-// Qx - Частное от деления Px на Mx
-// Rx - Остаток от деления
+// Произвоидтся деление a на b
 
 // Подключаемые модули:
 // DEG_P_N - Степень многочлена
 // DIV_QQ_Q - Деление дробей (делитель отличен от нуля)
-// ADD_PP_P - Сложение многочленов
-// SUB_PP_P - Вычитание многочленов
-// MUL_PP_P - Умножение многочленов
+// SUB_QQ_Q - Вычитание дробей
 
 // гр. 4305, Аллаяров, Хабиров
 
-#include "DEG_P_N.c"
-#include "DIV_QQ_Q .c"
-#include "ADD_PP_P.c"
-#include "SUB_PP_P.c"
-#include "MUL_PP_P.c"
-
-struct POLYNOMIAL DIV_PP_P(struct POLYNOMIAL Px, struct POLYNOMIAL Mx)
+struct POLYNOMIAL DIV_PP_P(struct POLYNOMIAL a, struct POLYNOMIAL b)
 {
-  struct POLYNOMIAL Qx;
-  Qx.factors = (RATIONAL*)malloc(sizeof(RATIONAL));
-  struct POLYNOMIAL Rx = Px;
-  struct POLYNOMIAL Tx;
-  Tx.factors = (RATIONAL*)malloc(sizeof(RATIONAL));
-  while(DEG_P_N (Rx) >= DEG_P_N (Mx))
-  {
-	  Tx.degree = (DEG_P_N (Rx) - DEG_P_N (Mx));
-	  Tx.factors[degree] = DIV_QQ_Q(Rx.factors[degree], Mx.factors[degree]);
-	  Qx = ADD_PP_P(Qx, Tx);
-	  Rx = SUB_PP_P(Rx, MUL_PP_P(Tx, Mx);
-  }
-  Tx.factors = NULL;
-  return Qx;
-}	
+	struct POLYNOMIAL res;
+	int i, j, k, n, m;
+	n = DEG_P_N(a); m = DEG_P_N(b);
+	k = n - m;
+	res.degree = k;
+	res.factors = (struct RATIONAL*)malloc((k + 1) * sizeof(struct RATIONAL));
+	for (; k >= 0; k--)
+	{
+		res.factors[k] = DIV_QQ_Q(a.factors[m + k], b.factors[m]);
+		for (i = m + k; j >= k; j--)
+			a.factors[j] = SUB_QQ_Q(a.factors[j], MUL_QQ_Q(res.factors[k], b.factors[j - k]));
+	}
+	return res;
+}
