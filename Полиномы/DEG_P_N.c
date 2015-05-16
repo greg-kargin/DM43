60211e793467864a04a6a49364cfa1e0d28c4ebc
adegtyarev33 <adegtyarev.pfn@gmail.com>
2015-05-16 04:11:08 +0400

    Update DEG_P_N.c
    Изменен алгоритм

--- Полиномы/DEG_P_N.c
+++ Полиномы/DEG_P_N.c
@@ -1,12 +1,9 @@
 //Подружаемые модули отсутствуют
 //Прототип: size_t DEG_P_N (struct POLYNOMIAL st); st - структура с массивом коэффициентов factors и старшей степенью degree
 //Возвращает степень многочлена по данному набору коэффициентов
-//Необходимо подключение malloc.h
 //Мясоедова, гр.4306
 
-#include <malloc.h>
-
-size_t DEG_P_N (struct POLYNOMIAL st)
-  {
-    return (_msize(st.factors)/sizeof(st.factors)-1); 
-  }
+int DEG_P_N(struct POLYNOMIAL st)
+{
+	return st.degree;
+}
